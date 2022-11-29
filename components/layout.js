56342141e4355231f73f8dbf53b26e045f2a6e87
gon2santos <necchigonzalo@gmail.com>
2022-11-29 11:47:23 -0300

    feat(style): stylized navbar and add social icons

--- components/layout.js
+++ components/layout.js
@@ -3,72 +3,71 @@ import Image from 'next/image';
 import styles from './layout.module.css';
 import utilStyles from '../styles/utils.module.css';
 import Link from 'next/link';
+import Divider from '@mui/material/Divider';
 
 const name = 'Gonzalo Dos Santos';
-export const siteTitle = 'Gonza Dos Santos';
+export const siteTitle = 'Gonzalo Dos Santos';
 
 export default function Layout({ children, home }) {
   return (
     <>
-    <div className={styles.container}>
-      <Head>
-        <link rel="icon" href="/favicon.ico" />
-        <meta
-          name="description"
-          content="Gonzalo Dos Santos Blog"
-        />
-        <meta
-          property="og:image"
-          content={`https://og-image.vercel.app/${encodeURI(
-            siteTitle,
-          )}.png?theme=light&md=0&fontSize=75px&images=https%3A%2F%2Fassets.vercel.com%2Fimage%2Fupload%2Ffront%2Fassets%2Fdesign%2Fnextjs-black-logo.svg`}
-        />
-        <meta name="og:title" content={siteTitle} />
-        <meta name="twitter:card" content="summary_large_image" />
-      </Head>
-      <header className={styles.header}>
-        {home ? (
-          <>
+      <div className={styles.navbar}>
+        <div className={styles.navbar_logo}>
+          <Link href="/">
             <Image
               priority
               src="/images/illuscrab.png"
               className={utilStyles.borderCircle}
-              height={144}
-              width={144}
+              height={90}
+              width={90}
               alt=""
             />
-            <h1 className={utilStyles.heading2Xl}>{name}</h1>
-          </>
-        ) : (
-          <>
-            <Link href="/">
-              <Image
-                priority
-                src="/images/illuscrab.png"
-                className={utilStyles.borderCircle}
-                height={108}
-                width={108}
-                alt=""
-              />
-            </Link>
-            <h2 className={utilStyles.headingLg}>
-              <Link href="/" className={utilStyles.colorInherit}>
-                {name}
-              </Link>
-            </h2>
-          </>
-        )}
-      </header>
-      <main>{children}</main>
-      {!home && (
-        <div className={styles.backToHome}>
-          <Link href="/">← Back to home</Link>
+          </Link>
         </div>
-      )}
-    </div>
-    <section>
-    <span className={utilStyles.smallText}>Illuscrab © 2022 by Gonzalo Dos Santos Necchi is licensed under CC BY-NC-ND 4.0</span>
-  </section>
+        <div className={styles.navbar_links}>
+          <span>About me</span>
+          <span>My Work</span>
+          <span>Contact</span>
+        </div>
+        <div className={styles.navbar_social}>
+          <Image priority src="/images/github.svg" height={24} width={24} alt="" />
+          <Image priority src="/images/linkedin.svg" height={24} width={24} alt="" />
+        </div>
+      </div>
+      <Divider variant="middle" />
+      <div className={styles.container}>
+        <Head>
+          <link rel="icon" href="/favicon.ico" />
+          <meta
+            name="description"
+            content="Gonzalo Dos Santos Blog"
+          />
+          <meta
+            property="og:image"
+            content={`https://og-image.vercel.app/${encodeURI(
+              siteTitle,
+            )}.png?theme=light&md=0&fontSize=75px&images=https%3A%2F%2Fassets.vercel.com%2Fimage%2Fupload%2Ffront%2Fassets%2Fdesign%2Fnextjs-black-logo.svg`}
+          />
+          <meta name="og:title" content={siteTitle} />
+          <meta name="twitter:card" content="summary_large_image" />
+        </Head>
+        <header className={styles.header}>
+          <h2 className={utilStyles.headingLg}>
+            <Link href="/" className={utilStyles.colorInherit}>
+              {name}
+            </Link>
+          </h2>
+        </header>
+        <main>{children}</main>
+        {!home && (
+          <div className={styles.backToHome}>
+            <Link href="/">← Back to home</Link>
+          </div>
+        )}
+      </div>
+      <section>
+        <span className={utilStyles.smallText}>Illuscrab © 2022 by Gonzalo Dos Santos Necchi is licensed under CC BY-NC-ND 4.0</span>
+      </section>
     </>
   );
 }
--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,14 @@
 import '../styles/global.css';
+import { Inter } from '@next/font/google';
+
+const inter = Inter({
+  subsets: ['latin'],
+}); 
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+  <main className={inter.className}>
+    <Component {...pageProps} />
+  </main>
+  )
 }
